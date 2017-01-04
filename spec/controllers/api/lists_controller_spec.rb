31a2c3405b78142952549d43f5838d115b6af255
aso4 <albertyso@gmail.com>
2017-01-03 22:58:19 -0500

    edit ListsController test with valid describe statement

--- spec/controllers/api/lists_controller_spec.rb
+++ spec/controllers/api/lists_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe ListsController, type: :controller do
+RSpec.describe Api::ListsController, type: :controller do
 
   describe "GET #edit" do
     it "returns http success" do
-      get :edit
+      get 'api/lists/:id/:edit'
       expect(response).to have_http_status(:success)
     end
   end
